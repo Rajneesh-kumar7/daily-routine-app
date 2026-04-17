8a7bb427df7acf924443e931ccab4d518dfbff01
Rajneesh-kumar7 <rajneeshqwer@gmail.com>
2026-04-17 11:52:31 +0530

    Refactor server.js to streamline task management

--- server.js
+++ server.js
@@ -8,14 +8,10 @@ app.use(express.static(__dirname));
 
 const FILE = path.join(__dirname, "tasks.json");
 
-// Load tasks from file
+// Load tasks
 let tasks = [];
 if (fs.existsSync(FILE)) {
-    try {
-        tasks = JSON.parse(fs.readFileSync(FILE, "utf-8"));
-    } catch {
-        tasks = [];
-    }
+    tasks = JSON.parse(fs.readFileSync(FILE, "utf-8"));
 }
 
 // Save tasks
@@ -23,24 +19,9 @@ function saveTasks() {
     fs.writeFileSync(FILE, JSON.stringify(tasks, null, 2));
 }
 
-// Live logging
-function logAction(action, data) {
-    const time = new Date().toLocaleString();
-    console.log(`[${time}] ${action}:`, data);
-}
-
-// Home route
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
 // Add task
 app.post("/add-task", (req, res) => {
     const { task } = req.body;
-
-    if (!task) {
-        return res.status(400).send("Task is required");
-    }
 
     const newTask = {
         id: Date.now(),
@@ -50,43 +31,18 @@ app.post("/add-task", (req, res) => {
 
     tasks.push(newTask);
     saveTasks();
-    logAction("Task Added", newTask);
+
+    console.log("Added:", newTask);
 
     res.send("Task Added");
 });
 
-// Get all tasks
+// Get tasks
 app.get("/tasks", (req, res) => {
     res.json(tasks);
-});
-
-// Update task
-app.post("/update-task", (req, res) => {
-    const { id } = req.body;
-
-    tasks = tasks.map(t =>
-        t.id == id ? { ...t, status: "completed" } : t
-    );
-
-    saveTasks();
-    logAction("Task Updated", id);
-
-    res.send("Task Updated");
-});
-
-// Delete task
-app.post("/delete-task", (req, res) => {
-    const { id } = req.body;
-
-    tasks = tasks.filter(t => t.id != id);
-    saveTasks();
-    logAction("Task Deleted", id);
-
-    res.send("Task Deleted");
 });
 
 // Start server
 app.listen(3000, "0.0.0.0", () => {
     console.log("Server running on port 3000");
-    console.log("Existing tasks:", tasks);
 });
